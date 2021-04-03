d84c4d0e044011b7507be7cbf33f1cd6a276e801
Nathan Zimmerberg <nhz2@cornell.edu>
2021-04-03 03:50:19 -0400

    ideal gas running on GPU?

--- simkernelproto.jl
+++ simkernelproto.jl
@@ -4,6 +4,7 @@ using CUDA
 using CUDAKernels
 using Random123
 using WormlikeChain
+using BenchmarkTools
 
 #BAOAB integration
 @kernel function simulate!(positions, 
@@ -13,11 +14,11 @@ using WormlikeChain
                           invβ,
                           VNbeads::Val{Nbeads},
                           stepnum::UInt64,
-                          rngkey::UInt64,
+                          Vrngkey::Val{rngkey},
                           Δt,
                           VNdims::Val{Ndims},
                           chainstarts::Val{Chstarts},
-                          VNsteps::Val{Nsteps}) where {Ndims, Nbeads, Chstarts, Nsteps}
+                          VNsteps::Val{Nsteps}) where {Ndims, Nbeads, rngkey, Chstarts, Nsteps}
     @uniform Nthreads = groupsize()[1]
     @uniform Nbeadsperthread= (Nbeads-1)÷Nthreads + 1
     @uniform Nrngsperbead= (Ndims-1)÷4 + 1
@@ -32,12 +33,12 @@ using WormlikeChain
         #Drift with O step half way
         @unroll for beadloopi in 1:Nbeadsperthread
             mybeadid= tid + (beadloopi-1)*Nthreads + (groupid-1)*Nbeads
-            randns= WormlikeChain.randn_32(rngkey, stepnum, (mybeadid<<32)%UInt64,VNdims)
-            for i in 1:Ndims
-                positions[mybeadid,i] += (Δt/2) * velocities[mybeadid,i]
-                velocities[mybeadid,i] *= exp(-Δt*γ)
-                velocities[mybeadid,i] += (√(1-exp(-2γ*Δt))*√(invβ*invmass)) * randns[i]
-                positions[mybeadid,i] += (Δt/2) * velocities[mybeadid,i]
+            randns= WormlikeChain.randn_32(rngkey, stepnum+step-1, (mybeadid<<32)%UInt64,VNdims)
+            @unroll for i in 1:Ndims
+                @inbounds positions[mybeadid,i] += (Δt/2) * velocities[mybeadid,i]
+                @inbounds velocities[mybeadid,i] *= exp(-Δt*γ)
+                @inbounds velocities[mybeadid,i] += (√(1-exp(-2γ*Δt))*√(invβ*invmass)) * randns[i]
+                @inbounds positions[mybeadid,i] += (Δt/2) * velocities[mybeadid,i]
             end
         end
         @synchronize
@@ -54,28 +55,28 @@ pos_d = CUDA.zeros(Float32,N,3)
 vel_h = zeros(Float32,N,3)
 vel_d = CUDA.zeros(Float32,N,3)
 
-@time wait(simkh(pos_h, 
-                vel_h,
-                1f0,
-                1f0/10f0,
-                1f0,
-                Val(N),
-                1%UInt64,
-                124432%UInt64,
-                1f0,
-                Val(3),
-                Val((1,)),
-                Val(1000),
-                 ndrange=N))
+# @time wait(simkh(pos_h, 
+#                 vel_h,
+#                 1f0,
+#                 1f0/10f0,
+#                 1f0,
+#                 Val(N),
+#                 1%UInt64,
+#                 124432%UInt64,
+#                 1f0,
+#                 Val(3),
+#                 Val((1,)),
+#                 Val(1000),
+#                  ndrange=N))
 
-@time wait(simkd(pos_d, 
+@btime wait(simkd(pos_d, 
                 vel_d,
                 1f0,
                 1f0/10f0,
                 1f0,
                 Val(N),
                 1%UInt64,
-                124432%UInt64,
+                Val(124432%UInt64),
                 1f0,
                 Val(3),
                 Val((1,)),
