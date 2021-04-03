c043ad7bdafda4a5a0846819f5ab9f234afc4dc8
Nathan Zimmerberg <nhz2@cornell.edu>
2021-04-03 03:00:05 -0400

    ci fix

--- simkernelproto.jl
+++ simkernelproto.jl
@@ -11,13 +11,13 @@ using WormlikeChain
                           invmass,
                           γ,
                           invβ,
-                          numbeads::Val{Nbeads},
+                          VNbeads::Val{Nbeads},
                           stepnum::UInt64,
                           rngkey::UInt64,
                           Δt,
-                          beadsize::Val{Ndims},
+                          VNdims::Val{Ndims},
                           chainstarts::Val{Chstarts},
-                          steps::Val{Nsteps}) where {Ndims, Nbeads, Chstarts, Nsteps}
+                          VNsteps::Val{Nsteps}) where {Ndims, Nbeads, Chstarts, Nsteps}
     @uniform Nthreads = groupsize()[1]
     @uniform Nbeadsperthread= (Nbeads-1)÷Nthreads + 1
     @uniform Nrngsperbead= (Ndims-1)÷4 + 1
@@ -27,23 +27,21 @@ using WormlikeChain
     #sharedA = @localmem Float32 N
     #@inbounds sharedA[I] = 0
     #TODO set up force calc shared mem
-    #for step in 1:Nsteps
+    for step in 1:Nsteps
         #Kick
         #Drift with O step half way
         @unroll for beadloopi in 1:Nbeadsperthread
             mybeadid= tid + (beadloopi-1)*Nthreads + (groupid-1)*Nbeads
-            @inbounds mypos= tuple((positions[mybeadid,i] for i in 1:Ndims)...)
-            @inbounds myvel= tuple((velocities[mybeadid,i] for i in 1:Ndims)...)
-            mypos = mypos .+ (Δt/2) .* myvel
-            # velocities[mybeadid,:] .*= exp(-Δt*γ)
-            # randns= WormlikeChain.randn_32(rngkey, stepnum, (mybeadid<<32)%UInt64,Ndims)
-            # velocities[mybeadid,:] .+= (√(1-exp(-2γ*Δt))*√(invβ*invmass)) .* randns
-            #mypos = mypos .+ (Δt/2) .* myvel
-            #for i in 1:Ndims; positions[mybeadid,i]= mypos[i]; end
-            #for i in 1:Ndims; velocities[mybeadid,i]= myvel[i]; end
+            randns= WormlikeChain.randn_32(rngkey, stepnum, (mybeadid<<32)%UInt64,VNdims)
+            for i in 1:Ndims
+                positions[mybeadid,i] += (Δt/2) * velocities[mybeadid,i]
+                velocities[mybeadid,i] *= exp(-Δt*γ)
+                velocities[mybeadid,i] += (√(1-exp(-2γ*Δt))*√(invβ*invmass)) * randns[i]
+                positions[mybeadid,i] += (Δt/2) * velocities[mybeadid,i]
+            end
         end
-        #@synchronize
-    #end
+        @synchronize
+    end
 end
 
 N=1024
@@ -56,19 +54,19 @@ pos_d = CUDA.zeros(Float32,N,3)
 vel_h = zeros(Float32,N,3)
 vel_d = CUDA.zeros(Float32,N,3)
 
-# @time wait(simkh(pos_h, 
-#                 vel_h,
-#                 1f0,
-#                 1f0/10f0,
-#                 1f0,
-#                 Val(N),
-#                 1%UInt64,
-#                 124432%UInt64,
-#                 1f0,
-#                 Val(3),
-#                 Val((1,)),
-#                 Val(1),
-#                  ndrange=N))
+@time wait(simkh(pos_h, 
+                vel_h,
+                1f0,
+                1f0/10f0,
+                1f0,
+                Val(N),
+                1%UInt64,
+                124432%UInt64,
+                1f0,
+                Val(3),
+                Val((1,)),
+                Val(1000),
+                 ndrange=N))
 
 @time wait(simkd(pos_d, 
                 vel_d,
@@ -81,5 +79,5 @@ vel_d = CUDA.zeros(Float32,N,3)
                 1f0,
                 Val(3),
                 Val((1,)),
-                Val(1),
+                Val(1000),
                  ndrange=N))
--- src/WormlikeChain.jl
+++ src/WormlikeChain.jl
@@ -4,7 +4,6 @@ module WormlikeChain
 
 # include stuff
 
-include("beads.jl")
 include("random_utils.jl")
 
 end
