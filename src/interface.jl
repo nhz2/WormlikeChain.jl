e721aa77a8d323424b06bf5ac16ae70f2cdca13a
nhz2 <nhz2@cornell.edu>
2021-04-13 21:00:29 -0400

    added global parameters

--- src/interface.jl
+++ src/interface.jl
@@ -10,60 +10,64 @@ struct BeadDefinition{Ndims}
     chain_pe
     chain_force
     perbead_param_keys
+    global_param_keys
     VNdims::Val{Ndims}
     chain_pe_expression
 end
 
-function BeadDefinition(chain_pe, param_keys::NTuple{Nparams,Symbol}, VNdims::Val{Ndims}) where {Ndims, Nparams}
+function BeadDefinition(chain_pe, perbead_param_keys::NTuple{Nbparams,Symbol}, global_param_keys::NTuple{Ngparams,Symbol} , VNdims::Val{Ndims}) where {Ndims, Nbparams, Ngparams}
     #symbolically diff potential energy to get force
     @variables p[1:Ndims]
     @variables b[1:Ndims]
     @variables n[1:Ndims]
-    @variables pars[1:Nparams]
-    symparams= (; zip(param_keys,(pars...,))...)
-    exprpe= chain_pe(p,b,n,symparams)
+    @variables bpars[1:Nbparams]
+    @variables gpars[1:Ngparams]
+    bsymparams= (; zip(perbead_param_keys,(bpars...,))...)
+    gsymparams= (; zip(global_param_keys,(gpars...,))...)
+    exprpe= chain_pe(p,b,n,bsymparams,gsymparams)
     g= Symbolics.gradient(-exprpe, [p;b;n]; simplify=true)
-    @info g
-    gexpr= build_function(g, [p;b;n], pars ,expression=Val{true})
+    @debug g
+    gexpr= build_function(g, [p;b;n], bpars, gpars ,expression=Val{true})
     gfun= eval(gexpr[1])
-    function chain_force(p,b,n, pars)
-        f= gfun([p;b;n], pars)
+    function chain_force(p,b,n, bpars, gpars)
+        f= gfun([p;b;n], bpars, gpars)
         pf = ntuple((i -> @inbounds(f[i])), VNdims)
         bf = ntuple((i -> @inbounds(f[i+Ndims])), VNdims)
         nf = ntuple((i -> @inbounds(f[i+2Ndims])), VNdims)
         (SVector(pf),SVector(bf),SVector(nf))
     end
-    BeadDefinition(chain_pe, chain_force, param_keys, VNdims, exprpe)
+    BeadDefinition(chain_pe, chain_force, perbead_param_keys, global_param_keys, VNdims, exprpe)
 end
 
 struct Chain
     beaddef::BeadDefinition
     perbead_params
+    global_params
     init_pos
     init_vel
-    function Chain(beaddef::BeadDefinition, perbead_params, init_pos, init_vel)
+    function Chain(beaddef::BeadDefinition, perbead_params, global_params, init_pos, init_vel)
         #error checking
         size(init_pos) == size(init_vel) || throw(DimensionMismatch("init_pos $(size(init_pos)) and init_vel $(size(init_vel)) don't match"))
         length(size(init_pos)) == 2 || throw(DimensionMismatch("init_pos $(length(size(init_pos))) must be 2"))
         Val(size(init_pos)[2]) == beaddef.VNdims || throw(DimensionMismatch("init_pos $(size(init_pos)) must match bead dimension $(beaddef.VNdims)"))
         Nbeads= size(init_pos)[1]
-        size(init_pos)[1] == length(perbead_params) || throw(DimensionMismatch("number of coords $(size(init_pos)[1]) must match number of params $(length(perbead_params))"))
+        Nbeads == length(perbead_params) || throw(DimensionMismatch("number of coords $(Nbeads) must match number of params $(length(perbead_params))"))
         #make sure forces are finite
         for beadi in 1:Nbeads
             p= init_pos[mod(beadi-2,Nbeads)+1,:]
             b= init_pos[beadi,:]
             n= init_pos[mod(beadi,Nbeads)+1,:]
-            pf, bf, nf = beaddef.chain_force(p,b,n,perbead_params[beadi])
-            e= beaddef.chain_pe(p,b,n,perbead_params[beadi])
+            pf, bf, nf = beaddef.chain_force(p,b,n,perbead_params[beadi], global_params)
+            e= beaddef.chain_pe(p,b,n,perbead_params[beadi], global_params)
             all(isfinite.([pf;bf;nf;e])) || @warn "some forces or energies are not finite on Bead number $(beadi)"
         end
-        new(beaddef::BeadDefinition, perbead_params, init_pos, init_vel)
+        new(beaddef::BeadDefinition, perbead_params, global_params, init_pos, init_vel)
     end
 end
 
 """
 zero velocities by default
 """
-function Chain(beaddef::BeadDefinition, perbead_params, init_pos)
-    Chain(beaddef::BeadDefinition, perbead_params, init_pos, zero(init_pos))
+function Chain(beaddef::BeadDefinition, perbead_params, global_params, init_pos)
+    Chain(beaddef::BeadDefinition, perbead_params, global_params, init_pos, zero(init_pos))
 end
--- test/test_interface.jl
+++ test/test_interface.jl
@@ -1,40 +1,44 @@
 @testset "BeadDefinition" begin
-    param_keys= (:k,:kθ)
-    function chain_pe(p,b,n,params)
+    perbead_param_keys= (:k,:kθ)
+    global_param_keys= (:d₀,)
+    function chain_pe(p,b,n,perbead_params,global_params)
         rp= (p .- b)
         rn= (n .- b)
         dp= sqrt(sum(rp.^2))
         dn= sqrt(sum(rn.^2))
         cosθ= sum(rp .* rn)/dp/dn
-        params.k*1//2*((dn-1)^2) + params.kθ*(cosθ+1)
+        perbead_params.k*1//2*((dn-global_params.d₀)^2) + perbead_params.kθ*(cosθ+1)
     end
-    beaddef1= BeadDefinition(chain_pe, param_keys, Val(3))
+    beaddef1= BeadDefinition(chain_pe, perbead_param_keys, global_param_keys, Val(3))
     p= [0.0, 0.0, 0.0]
     b= [0.0, 0.0, 1.0]
     n= [0.0, 0.0, 2.0]
     params= (k=3.5, kθ=4.4)
-    @test 0.0 == beaddef1.chain_pe(p,b,n,params)
-    fp, fb, fn = beaddef1.chain_force(p,b,n,params)
+    globalparams= (d₀=1.0, )
+    @test 0.0 == beaddef1.chain_pe(p,b,n,params, globalparams)
+    fp, fb, fn = beaddef1.chain_force(p,b,n,params, globalparams)
     @test 0.0 == norm(fp)
     @test 0.0 == norm(fb)
     @test 0.0 == norm(fn)
 end
 
 @testset "Chain" begin
-    param_keys= (:k,:kθ)
-    function chain_pe(p,b,n,params)
+    perbead_param_keys= (:k,:kθ)
+    global_param_keys= (:d₀,)
+    function chain_pe(p,b,n,perbead_params,global_params)
         rp= (p .- b)
         rn= (n .- b)
         dp= sqrt(sum(rp.^2))
         dn= sqrt(sum(rn.^2))
         cosθ= sum(rp .* rn)/dp/dn
-        params.k*1//2*((dn-1)^2) + params.kθ*(cosθ+1)
+        perbead_params.k*1//2*((dn-global_params.d₀)^2) + perbead_params.kθ*(cosθ+1)
     end
-    beaddef1= BeadDefinition(chain_pe, param_keys, Val(3))
+    beaddef1= BeadDefinition(chain_pe, perbead_param_keys, global_param_keys, Val(3))
     params= fill((k=3.5, kθ=4.4),10)
     params[10]= (k=0.0, kθ= 0.0)# turn off interation to split ends
     params[1]= (k=3.5, kθ= 0.0)
     pos= zeros(10,3)
     pos[:,3] .= range(0.0, step= 1.0, length=10)
-    Chain(beaddef1,params, pos)
+    globalparams= (d₀=1.0, )
+    Chain(beaddef1,params, globalparams, pos)
 end
